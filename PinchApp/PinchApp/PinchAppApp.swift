89b08b54f9fb1c97f269c257a4fd518d20ab286a
Sheep1sik <bohg6799@naver.com>
2024-02-02 14:02:22 +0900

    Feat: Setting Project

---
+++ PinchApp/PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by 양원식 on 2024/02/02.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
