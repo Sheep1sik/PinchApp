b89b5853488d7884b0a5fffd892b40920d24c16d
Sheep1sik <bohg6799@naver.com>
2024-02-05 16:25:02 +0900

    Feat: Control Interface 구현

---
+++ PinchApp/PinchApp/View/COntrolImageView.swift
@@ -0,0 +1,26 @@
+//
+//  ControlImageView.swift
+//  PinchApp
+//
+//  Created by 양원식 on 2024/02/05.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView(icon: "minus.magnifyingglass")
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
