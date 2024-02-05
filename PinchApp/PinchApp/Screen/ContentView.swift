70d0822470ca31f640ed025cfc668b92b2631b04
Sheep1sik <bohg6799@naver.com>
2024-02-05 14:55:32 +0900

    Feat: Double Tap Gesture 구현

--- PinchApp/PinchApp/Screen/ContentView.swift
+++ PinchApp/PinchApp/Screen/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTY
+    
+    @State private var isAnimatiog: Bool = false
+    @State private var imageScale: CGFloat = 1
+    // MARK: - FUNCTION
+    
+    // MARK: - CONTENT
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            ZStack{
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimatiog ? 1 : 0)
+                    .scaleEffect(imageScale)
+                // MARK: - 1. TAP GESTURE
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    })
+            } //: ZSTACK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimatiog = true
+                }
+            })
+        } //: NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
 
@@ -23,5 +58,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .preferredColorScheme(.dark)
+            .previewDevice("iPhone 13")
     }
 }
